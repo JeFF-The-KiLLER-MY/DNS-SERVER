3cb144a60dcf17ee79b63129c81702d4c9f96800
JeFF-The-KiLLER-MY <xohanchowdhury.one1@gmail.com>
2025-09-19 04:25:22 +0800

    Add files via upload

---
+++ DNS.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from socketserver import UDPServer, BaseRequestHandler
+from dnslib import DNSRecord, RR, QTYPE, A
+import time
+
+# -----------------------------
+# Configuration
+# -----------------------------
+HOST = "0.0.0.0"   # listen on all interfaces
+PORT = 5353       # high port so we don't need sudo
+RESPONSE_IP = "127.0.0.1"  # IP to return for every query
+
+class DNSHandler(BaseRequestHandler):
+    def handle(self):
+        data, sock = self.request
+        request = DNSRecord.parse(data)
+        print(f"[{time.strftime('%H:%M:%S')}] Query for {request.q.qname}")
+        reply = request.reply()
+        reply.add_answer(RR(
+            rname=request.q.qname,
+            rtype=QTYPE.A,
+            rclass=1,
+            ttl=60,
+            rdata=A(RESPONSE_IP)
+        ))
+        sock.sendto(reply.pack(), self.client_address)
+
+if __name__ == "__main__":
+    print(f"Starting DNS server on {HOST}:{PORT}, answering with {RESPONSE_IP}")
+    with UDPServer((HOST, PORT), DNSHandler) as server:
+        server.serve_forever()
